eeb65b05899500e8d33bae5e709ac8cfc3cef30a
Daniel Klabbers <daniel+hyn@klabbers.email>
2016-02-26 13:39:18 +0100

    added directory check

--- src/Commands/AddCommand.php
+++ src/Commands/AddCommand.php
@@ -8,7 +8,8 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class AddCommand extends Command {
+class AddCommand extends Command
+{
     /**
      * {@inheritdoc}
      */
@@ -17,17 +18,23 @@ class AddCommand extends Command {
         $this->setName('add')
             ->setDescription('Add a directory to your memory.')
             ->addArgument('name', InputArgument::REQUIRED, 'The name to remember this item, supports array dot.')
-            ->addOption('path', 'p', InputOption::VALUE_OPTIONAL, 'The path to add, if left out the current directory will be assumed.', getcwd())
-            ;
+            ->addOption('path', 'p', InputOption::VALUE_OPTIONAL, 'The path to add, if left out the current directory will be assumed.', getcwd());
     }
 
     /**
      * @param InputInterface  $input
      * @param OutputInterface $output
+     * @return int|null|void
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $directory = new Directory();
+        if (!realpath($input->getOption('path'))) {
+            $output->writeln('<error>--path value does not point to a valid location</error>');
+
+            return;
+        }
+
+        $directory       = new Directory();
         $directory->path = $input->getOption('path');
         $directory->name = $input->getArgument('name');
         $directory->save();
